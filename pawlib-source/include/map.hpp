994b7470e572f4d7a1ca0ddba78848798ec54896
Jason C. McDonald <jcmcdonald@mousepawgames.com>
2016-04-16 19:55:04 -0700

    Add license and title comments to the rest of the header files.

--- pawlib-source/include/map.hpp
+++ pawlib-source/include/map.hpp
@@ -1,3 +1,39 @@
+/** Map [PawLIB]
+  * Version: 1.0
+  *
+  * Allows managed, custom output to any console or terminal.
+  * See WHAT IS IOCHANNEL? below.
+  *
+  * Last Updated: 8 March 2016
+  * Author: Michael Parkman
+  */
+
+/* LICENSE
+ * Copyright (C) 2016 MousePaw Games.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * CONTRIBUTING
+ * See http://www.mousepawgames.com/participate/opensource for information
+ * on how to contribute to our projects.
+ */
+
 #ifndef MAP_HPP_INCLUDED
 #define MAP_HPP_INCLUDED
 
