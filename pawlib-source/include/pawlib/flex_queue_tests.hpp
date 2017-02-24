0b0c9d1cbcaefecb57a28f8b227981aa3e837828
Jason C. McDonald <jcmcdonald@mousepawgames.com>
2017-02-24 14:25:39 -0800

    Updated copyright info.

--- pawlib-source/include/pawlib/flex_queue_tests.hpp
+++ pawlib-source/include/pawlib/flex_queue_tests.hpp
@@ -5,7 +5,7 @@
   */
 
 /* LICENSE
- * Copyright (c) 2016 MousePaw Games.
+ * Copyright (c) 2016 MousePaw Media.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
