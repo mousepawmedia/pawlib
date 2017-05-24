96164b8c329ebbe864188c8f78a39ee72d6ab11c
Jason C. McDonald <jcmcdonald@mousepawgames.com>
2017-05-24 12:36:17 -0700

    @nsgroggett changes (computer ate them in transit?)

--- pawlib-source/include/pawlib/base_flex_array.hpp
+++ pawlib-source/include/pawlib/base_flex_array.hpp
@@ -52,9 +52,6 @@
 
 #include "pawlib/iochannel.hpp"
 
-#define UNIT32_MAX (0xffffffff) //this is a temporary fix to allow for capacity check
-
-
 using pawlib::iochannel;
 using namespace pawlib::ioformat;
 
@@ -216,7 +213,7 @@ namespace pawlib
                 }
 
                 //check to see if maximum size is being approached
-                if(cpacity > UINT32_MAX/2)
+                if(capacity >= UINT32_MAX/2)
                 {
                   //set it to limit defined by UINT32_MAX
                   capacity = UINT32_MAX;
